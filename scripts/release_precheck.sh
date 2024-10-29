21c1c06e94962b1701831428aadfed9d10b59c79
Guillaume Perrin <guillaume28.perrin@gmail.com>
2024-10-29 09:08:46 +0100

    Add CI
    Signed-off-by: Guillaume Perrin <guillaume28.perrin@gmail.com>

---
+++ scripts/release_precheck.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e # exit on first error
+
+function check_git_status() {
+if [[ -z $(git status --porcelain) ]]
+then
+  echo "ok"
+else
+  echo "Found modified files:"
+  git status --porcelain
+  exit 1
+fi
+}
+
+check_git_status
+go mod tidy
+make fmt
+check_git_status
+
+make static-analysis
